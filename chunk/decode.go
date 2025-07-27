7f8e657d9f605516c556611bdf54bfa6da630a66
Happy2018new <chenbo79800@163.com>
2025-07-27 19:31:23 +0800

    Fix DecodeSubChunk

--- chunk/decode.go
+++ chunk/decode.go
@@ -3,51 +3,62 @@ package chunk
 import (
 	"bytes"
 	"fmt"
+
+	block_general "github.com/TriM-Organization/dream-weaver-factory/block/general"
+	"github.com/TriM-Organization/dream-weaver-factory/define"
 )
 
 // DecodeSubChunk decodes a SubChunk from a bytes.Buffer.
-// The Encoding passed defines how the block storages of the SubChunk are decoded.
-func DecodeSubChunk(buf *bytes.Buffer, c *Chunk, index *byte, e Encoding, upgradeType int) (*SubChunk, error) {
+// The Encoding passed defines how the block storages of the
+// SubChunk are decoded.
+func DecodeSubChunk(buf *bytes.Buffer, r define.Range, e Encoding, upgradeType int) (subChunk *SubChunk, index int, err error) {
+	var airRuntimeID uint32
+	switch upgradeType {
+	case UpgradeTypeToLower:
+		airRuntimeID = block_general.NEMCAirRuntimeID
+	default:
+		airRuntimeID = block_general.StdAirRuntimeID
+	}
 	ver, err := buf.ReadByte()
 	if err != nil {
-		return nil, fmt.Errorf("error reading version: %w", err)
+		return nil, 255, fmt.Errorf("error reading version: %w", err)
 	}
-	sub := NewSubChunk(c.air)
+	sub := NewSubChunk(airRuntimeID)
 	switch ver {
 	default:
-		return nil, fmt.Errorf("unknown sub chunk version %v: can't decode", ver)
+		return nil, 255, fmt.Errorf("unknown sub chunk version %v: can't decode", ver)
 	case 1:
 		// Version 1 only has one layer for each sub chunk, but uses the format with palettes.
 		storage, err := decodePalettedStorage(buf, e, upgradeType)
 		if err != nil {
-			return nil, err
+			return nil, 255, err
 		}
 		sub.storages = append(sub.storages, storage)
 	case 8, 9:
 		// Version 8 allows up to 256 layers for one sub chunk.
 		storageCount, err := buf.ReadByte()
 		if err != nil {
-			return nil, fmt.Errorf("error reading storage count: %w", err)
+			return nil, 255, fmt.Errorf("error reading storage count: %w", err)
 		}
 		if ver == 9 {
 			uIndex, err := buf.ReadByte()
 			if err != nil {
-				return nil, fmt.Errorf("error reading sub-chunk index: %w", err)
+				return nil, 255, fmt.Errorf("error reading sub-chunk index: %w", err)
 			}
 			// The index as written here isn't the actual index of the sub-chunk within the chunk. Rather, it is the Y
 			// value of the sub-chunk. This means that we need to translate it to an index.
-			*index = uint8(int8(uIndex) - int8(c.r[0]>>4))
+			index = int(int8(uIndex) - int8(r[0]>>4))
 		}
 		sub.storages = make([]*PalettedStorage, storageCount)
 
 		for i := range storageCount {
 			sub.storages[i], err = decodePalettedStorage(buf, e, upgradeType)
 			if err != nil {
-				return nil, err
+				return nil, 255, err
 			}
 		}
 	}
-	return sub, nil
+	return sub, index, nil
 }
 
 // decodePalettedStorage decodes a PalettedStorage from a bytes.Buffer. The Encoding passed is used to read either a
